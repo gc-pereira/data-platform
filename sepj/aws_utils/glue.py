c4ca4374ec63db69b900fc226b7adacff03b75d9
gc-pereira <carvalho.gabrielp@gmail.com>
2024-06-16 22:28:47 -0300

    feat(Cloud): create cloud manager data plataform

---
+++ sepj/aws_utils/glue.py
@@ -0,0 +1,16 @@
+class GlueManager:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def update_table(self):
+        ...
+        
+    def create_table(self):
+        ...
+        
+    def create_table_by_metadata(self):
+        ...
+    
+    def create_table_by_dataframe_types(self, df):
+        ...
