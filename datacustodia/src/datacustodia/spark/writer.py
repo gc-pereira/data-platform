d5098b3d8e44f4a52c8edcb0cfc24f9e001bb1f7
gc-pereira <carvalho.gabrielp@gmail.com>
2025-11-28 20:54:20 -0300

    chore(spark): otimizando job

--- datacustodia/src/datacustodia/spark/writer.py
+++ datacustodia/src/datacustodia/spark/writer.py
@@ -7,7 +7,7 @@ class SparkWriter:
     def write_partitioned(self, df: DataFrame, path: str, partition_cols: list):
         df = df.withColumn('last_update_date', lit(datetime.today()))
         (
-            df.write
+            df.repartition(1).write
             .mode("overwrite")
             .partitionBy(partition_cols)
             .parquet(path)
